652c9fa027d62aeb09bb32809995c66335a55755
ishomayo <esguerraeugene98@gmail.com>
2025-02-18 18:33:21 +0800

    sounds

--- Hangover/src/Constants.java
+++ Hangover/src/Constants.java
@@ -5,9 +5,6 @@ public class Constants {
     public static final String SPLASH_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Loading Screen.mp4";
     public static final String LOBBY_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Background_Lobby.mp4";
     public static final String IMAGE_PATH = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\Title.png";
-    public static final String SPLASH_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Loading Screen.mp4";
-    public static final String LOBBY_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Background_Lobby.mp4";
-    public static final String IMAGE_PATH = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\Title.png";
     public static final int FADE_DURATION = 1000;
     public static final Dimension FRAME_SIZE = new Dimension(1000, 600);
 
