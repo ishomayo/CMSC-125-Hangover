cb86b942595d10a6c0890230754666865d130281
ishomayo <esguerraeugene98@gmail.com>
2025-02-18 14:05:26 +0800

    Update Constants.java

--- Hangover/src/Constants.java
+++ Hangover/src/Constants.java
@@ -4,6 +4,42 @@ public class Constants {
     public static final String SPLASH_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Loading Screen.mp4";
     public static final String LOBBY_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Background_Lobby.mp4";
     public static final String IMAGE_PATH = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\Title.png";
+    public static final String IMG_START = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\start.png";
+    public static final String IMG_START_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\start-hover.png";
+    public static final String IMG_START_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\start-click.png";
+    public static final String IMG_HOW_TO = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\how_to.png";
+    public static final String IMG_HOW_TO_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\how_to-hover.png";
+    public static final String IMG_HOW_TO_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\how_to-click.png";
+    public static final String IMG_CREDITS = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\credits.png";
+    public static final String IMG_CREDITS_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\credits-hover.png";
+    public static final String IMG_CREDITS_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\credits-click.png";
+    public static final String IMG_EXIT = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\exit.png";
+    public static final String IMG_EXIT_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\exit-hover.png";
+    public static final String IMG_EXIT_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\exit-click.png";
+    public static final String IMG_MUSIC = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\music-on.png";
+    public static final String IMG_SFX = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\sfx-on.png";
+    
+    public static final String IMG_EASY = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\easy.png";
+    public static final String IMG_EASY_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\easy-hover.png";
+    public static final String IMG_EASY_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\easy-click.png";
+
+    
+    public static final String IMG_AVERAGE = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\avg.png";
+    public static final String IMG_AVERAGE_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\avg-hover.png";
+    public static final String IMG_AVERAGE_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\avg-click.png";
+    
+    public static final String IMG_DIFFICULT = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\diff.png";
+    public static final String IMG_DIFFICULT_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\diff-hover.png";
+    public static final String IMG_DIFFICULT_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\diff-click.png";
+    
+    public static final String IMG_RETURN = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\return.png";
+    public static final String IMG_RETURN_HOVER = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\return-hover.png";
+    public static final String IMG_RETURN_CLICK = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\return-click.png";
+
+    public static final String IMG_CREDITS_SCREEN = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\return-click.png";
+
+    public static final String IMG_HOW_TO_PLAY = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\return-click.png";
+
     public static final int FADE_DURATION = 1000;
     public static final Dimension FRAME_SIZE = new Dimension(1000, 600);
 }
