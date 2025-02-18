65d82dcc5c3430d3094bb14f2a42d4cbf8f84f99
Euvan Dave Siervo <58839850+essiervo@users.noreply.github.com>
2025-02-18 15:11:00 +0800

    Add files via upload
    Fixed directories

--- Hangover/src/Constants.java
+++ Hangover/src/Constants.java
@@ -1,45 +1,45 @@
-import java.awt.Dimension;
+import java.awt.*;
 
 public class Constants {
-    private static final String BASE_PATH = "C:\\Users\\Eugene\\Desktop\\Git\\CMSC-125-Hangover\\Hangover\\resources\\";
-
-    public static final String SPLASH_VIDEO_PATH = BASE_PATH + "Loading Screen.mp4";
-    public static final String LOBBY_VIDEO_PATH = BASE_PATH + "Background_Lobby.mp4";
-    public static final String IMAGE_PATH = BASE_PATH + "Title.png";
-    public static final String IMG_START = BASE_PATH + "start.png";
-    public static final String IMG_START_HOVER = BASE_PATH + "start-hover.png";
-    public static final String IMG_START_CLICK = BASE_PATH + "start-click.png";
-    public static final String IMG_HOW_TO = BASE_PATH + "how_to.png";
-    public static final String IMG_HOW_TO_HOVER = BASE_PATH + "how_to-hover.png";
-    public static final String IMG_HOW_TO_CLICK = BASE_PATH + "how_to-click.png";
-    public static final String IMG_CREDITS = BASE_PATH + "credits.png";
-    public static final String IMG_CREDITS_HOVER = BASE_PATH + "credits-hover.png";
-    public static final String IMG_CREDITS_CLICK = BASE_PATH + "credits-click.png";
-    public static final String IMG_EXIT = BASE_PATH + "exit.png";
-    public static final String IMG_EXIT_HOVER = BASE_PATH + "exit-hover.png";
-    public static final String IMG_EXIT_CLICK = BASE_PATH + "exit-click.png";
-    public static final String IMG_MUSIC = BASE_PATH + "music-on.png";
-    public static final String IMG_SFX = BASE_PATH + "sfx-on.png";
-
-    public static final String IMG_EASY = BASE_PATH + "easy.png";
-    public static final String IMG_EASY_HOVER = BASE_PATH + "easy-hover.png";
-    public static final String IMG_EASY_CLICK = BASE_PATH + "easy-click.png";
-
-    public static final String IMG_AVERAGE = BASE_PATH + "avg.png";
-    public static final String IMG_AVERAGE_HOVER = BASE_PATH + "avg-hover.png";
-    public static final String IMG_AVERAGE_CLICK = BASE_PATH + "avg-click.png";
-
-    public static final String IMG_DIFFICULT = BASE_PATH + "diff.png";
-    public static final String IMG_DIFFICULT_HOVER = BASE_PATH + "diff-hover.png";
-    public static final String IMG_DIFFICULT_CLICK = BASE_PATH + "diff-click.png";
-
-    public static final String IMG_RETURN = BASE_PATH + "return.png";
-    public static final String IMG_RETURN_HOVER = BASE_PATH + "return-hover.png";
-    public static final String IMG_RETURN_CLICK = BASE_PATH + "return-click.png";
-
-    public static final String IMG_CREDITS_SCREEN = BASE_PATH + "credits-screen.png";  // Update if necessary
-    public static final String IMG_HOW_TO_PLAY = BASE_PATH + "how-to-play.png";  // Update if necessary
-
+    public static final String SPLASH_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Loading Screen.mp4";
+    public static final String LOBBY_VIDEO_PATH = "D:\\125 Hangman\\CMSC-125-Hangover\\Hangover\\resources\\Background_Lobby.mp4";
+    public static final String IMAGE_PATH = "D:\\125 Hangman\\\\CMSC-125-Hangover\\Hangover\\resources\\Title.png";
     public static final int FADE_DURATION = 1000;
     public static final Dimension FRAME_SIZE = new Dimension(1000, 600);
+
+    // Lobby Buttons
+    public static final String IMG_START = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/start.png";
+    public static final String IMG_START_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/start-hover.png";
+    public static final String IMG_START_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/start-click.png";
+    public static final String IMG_HOW_TO = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/how_to.png";
+    public static final String IMG_HOW_TO_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/how_to-hover.png";
+    public static final String IMG_HOW_TO_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/how_to-click.png";
+    public static final String IMG_CREDITS = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/credits.png";
+    public static final String IMG_CREDITS_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/credits-hover.png";
+    public static final String IMG_CREDITS_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/credits-click.png";
+    public static final String IMG_EXIT = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/exit.png";
+    public static final String IMG_EXIT_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/exit-hover.png";
+    public static final String IMG_EXIT_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/exit-click.png";
+    public static final String IMG_MUSIC = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/music-on.png";
+    public static final String IMG_SFX = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/sfx-on.png";
+
+    // Select Category Buttons
+    public static final String IMG_EASY = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/easy.png";
+    public static final String IMG_EASY_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/easy-hover.png";
+    public static final String IMG_EASY_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/easy-click.png";
+    public static final String IMG_AVERAGE = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/avg.png";
+    public static final String IMG_AVERAGE_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/avg-hover.png";
+    public static final String IMG_AVERAGE_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/avg-click.png";
+    public static final String IMG_DIFFICULT = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/diff.png";
+    public static final String IMG_DIFFICULT_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/diff-hover.png";
+    public static final String IMG_DIFFICULT_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/diff-click.png";
+
+    // Return Button
+    public static final String IMG_RETURN = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/return.png";
+    public static final String IMG_RETURN_HOVER = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/return-hover.png";
+    public static final String IMG_RETURN_CLICK = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/return-click.png";
+
+    // How to Play and Credits Screens
+    public static final String IMG_HOW_TO_PLAY = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/HowToPlay.png";
+    public static final String IMG_CREDITS_SCREEN = "file:/D:/125 Hangman/CMSC-125-Hangover/Hangover/resources/CreditsScreen.png";
 }
